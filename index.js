e6711d34f7020a14c510b433549c4a3837508319
ArzyxMC <152070301+ArzyxMC@users.noreply.github.com>
2024-02-25 13:52:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["What you lookin' at blud?"];
 
 
 let currentIndex = 0;
